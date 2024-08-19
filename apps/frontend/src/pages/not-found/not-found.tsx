34e03c33b253f5428256113b8572086d1c689cb9
TheDaidor <danielcastillo1552001@hotmail.com>
2024-08-19 15:07:08 +0100

    fix: update not found page gf-20

--- apps/frontend/src/pages/not-found/not-found.tsx
+++ apps/frontend/src/pages/not-found/not-found.tsx
@@ -1,18 +1,16 @@
-import { Link } from "react-router-dom";
+import { Button } from "~/libs/components/components.js";
 
 import styles from "./styles.module.css";
 
 const NotFound = (): JSX.Element => {
 	return (
 		<main className={styles["container"]}>
-			<h1 className={styles["title"]}>404</h1>
-			<h3 className={styles["subtitle"]}>Something went wrong</h3>
+			<span className={styles["title"]}>404</span>
+			<h1 className={styles["subtitle"]}>Something went wrong</h1>
 			<p className={styles["text"]}>
 				Sorry, we can’t find the page you’re looking for.
 			</p>
-			<Link className={styles["link"]} to="/">
-				Back to home
-			</Link>
+			<Button className={styles["link"]} href="/" label="Back to home" />
 		</main>
 	);
 };
