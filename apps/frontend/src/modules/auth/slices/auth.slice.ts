19d4f3ca0034f183ded9738db35466edc07c100b
sandrvvu <sandrvvu@gmail.com>
2024-08-19 13:51:51 +0300

    feat: add authenticated user to auth slice gf-14

--- apps/frontend/src/modules/auth/slices/auth.slice.ts
+++ apps/frontend/src/modules/auth/slices/auth.slice.ts
@@ -3,13 +3,16 @@ import { createSlice } from "@reduxjs/toolkit";
 import { DataStatus } from "~/libs/enums/enums.js";
 import { type ValueOf } from "~/libs/types/types.js";
 
+import { type UserSignUpResponseDto } from "../libs/types/types.js";
 import { signUp } from "./actions.js";
 
 type State = {
+	authenticatedUser: null | UserSignUpResponseDto;
 	dataStatus: ValueOf<typeof DataStatus>;
 };
 
 const initialState: State = {
+	authenticatedUser: null,
 	dataStatus: DataStatus.IDLE,
 };
 
