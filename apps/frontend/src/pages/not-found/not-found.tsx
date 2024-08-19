c527c2846c4acf238d22baee61025f8080d5225f
TheDaidor <danielcastillo1552001@hotmail.com>
2024-08-19 11:11:58 +0100

    feat: add not found page gf-20

---
+++ apps/frontend/src/pages/not-found/not-found.tsx
@@ -0,0 +1,20 @@
+import { Link } from "react-router-dom";
+
+import styles from "./styles.module.css";
+
+const NotFound = (): JSX.Element => {
+	return (
+		<section className={styles["not-found"]}>
+			<h1 className={styles["title"]}>404</h1>
+			<h3 className={styles["subtitle"]}>Something went wrong</h3>
+			<p className={styles["text"]}>
+				Sorry, we can’t find the page you’re looking for.
+			</p>
+			<Link className={styles["link"]} to="/">
+				Back to home
+			</Link>
+		</section>
+	);
+};
+
+export { NotFound };
