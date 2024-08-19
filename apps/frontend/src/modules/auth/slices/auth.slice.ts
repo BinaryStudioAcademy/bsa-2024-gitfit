f4d16a5f8f7dd58c47ee72a80edf33f2f6754e29
kolibri753 <kolibri753@gmail.com>
2024-08-19 18:08:52 +0300

    refactor: update auth slice to store user data after sign up gf-5

--- apps/frontend/src/modules/auth/slices/auth.slice.ts
+++ apps/frontend/src/modules/auth/slices/auth.slice.ts
@@ -2,15 +2,18 @@ import { createSlice } from "@reduxjs/toolkit";
 
 import { DataStatus } from "~/libs/enums/enums.js";
 import { type ValueOf } from "~/libs/types/types.js";
+import { type UserSignUpResponseDto } from "~/modules/users/users.js";
 
 import { signUp } from "./actions.js";
 
 type State = {
 	dataStatus: ValueOf<typeof DataStatus>;
+	user: null | UserSignUpResponseDto;
 };
 
 const initialState: State = {
 	dataStatus: DataStatus.IDLE,
+	user: null,
 };
 
 const { actions, name, reducer } = createSlice({
@@ -18,8 +21,9 @@ const { actions, name, reducer } = createSlice({
 		builder.addCase(signUp.pending, (state) => {
 			state.dataStatus = DataStatus.PENDING;
 		});
-		builder.addCase(signUp.fulfilled, (state) => {
+		builder.addCase(signUp.fulfilled, (state, action) => {
 			state.dataStatus = DataStatus.FULFILLED;
+			state.user = action.payload;
 		});
 		builder.addCase(signUp.rejected, (state) => {
 			state.dataStatus = DataStatus.REJECTED;
