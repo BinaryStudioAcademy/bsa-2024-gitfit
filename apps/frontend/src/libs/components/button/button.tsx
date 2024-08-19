2aa26cf7ca1621c039b0845159555da8a7c385e0
TheDaidor <danielcastillo1552001@hotmail.com>
2024-08-19 15:05:33 +0100

    feat: add href parameter gf-20

--- apps/frontend/src/libs/components/button/button.tsx
+++ apps/frontend/src/libs/components/button/button.tsx
@@ -1,10 +1,31 @@
+import { NavLink } from "react-router-dom";
+
 type Properties = {
+	className?: string | undefined;
+	href?: string | undefined;
 	label: string;
 	type?: "button" | "submit";
 };
 
-const Button = ({ label, type = "button" }: Properties): JSX.Element => (
-	<button type={type}>{label}</button>
-);
+const Button = ({
+	className = "",
+	href,
+	label,
+	type = "button",
+}: Properties): JSX.Element => {
+	if (href) {
+		return (
+			<NavLink className={className} to={href}>
+				{label}
+			</NavLink>
+		);
+	}
+
+	return (
+		<button className={className} type={type}>
+			{label}
+		</button>
+	);
+};
 
 export { Button };
