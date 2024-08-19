d0c011ce0f83de49a5e42f94fb9d1d1f8b48a40c
TheDaidor <danielcastillo1552001@hotmail.com>
2024-08-19 16:40:06 +0100

    feat: change not found styles gf-20

--- apps/frontend/src/pages/not-found/not-found.tsx
+++ apps/frontend/src/pages/not-found/not-found.tsx
@@ -5,12 +5,12 @@ import styles from "./styles.module.css";
 const NotFound = (): JSX.Element => {
 	return (
 		<main className={styles["container"]}>
-			<span className={styles["title"]}>404</span>
-			<h1 className={styles["subtitle"]}>Something went wrong</h1>
+			<span className={styles["decoration"]}>404</span>
+			<h1 className={styles["title"]}>Something went wrong</h1>
 			<p className={styles["text"]}>
 				Sorry, we can’t find the page you’re looking for.
 			</p>
-			<Button className={styles["link"]} href="/" label="Back to home" />
+			<Button href="/" label="Back to home" />
 		</main>
 	);
 };
