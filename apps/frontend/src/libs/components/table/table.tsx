ceaf0440474655aa23162cdca94ff724616d5c8f
AnutkaGn <annaartemivna@gmail.com>
2024-08-19 23:01:33 +0300

    feat: create a table component gf-22

---
+++ apps/frontend/src/libs/components/table/table.tsx
@@ -0,0 +1,58 @@
+import {
+	type ColumnDef,
+	flexRender,
+	getCoreRowModel,
+	useReactTable,
+} from "@tanstack/react-table";
+
+import styles from "./styles.module.css";
+
+interface TableProperties<T extends object> {
+	columns: ColumnDef<T>[];
+	data: T[];
+}
+
+const Table = <T extends object>({
+	columns,
+	data,
+}: TableProperties<T>): JSX.Element => {
+	const table = useReactTable({
+		columns,
+		data,
+		getCoreRowModel: getCoreRowModel(),
+	});
+
+	return (
+		<div className={styles["table-container"]}>
+			<table className={styles["table"]}>
+				<thead>
+					{table.getHeaderGroups().map((headerGroup) => (
+						<tr key={headerGroup.id}>
+							{headerGroup.headers.map((header) => (
+								<th key={header.id}>
+									{flexRender(
+										header.column.columnDef.header,
+										header.getContext(),
+									)}
+								</th>
+							))}
+						</tr>
+					))}
+				</thead>
+				<tbody>
+					{table.getRowModel().rows.map((row) => (
+						<tr key={row.id}>
+							{row.getVisibleCells().map((cell) => (
+								<td key={cell.id}>
+									{flexRender(cell.column.columnDef.cell, cell.getContext())}
+								</td>
+							))}
+						</tr>
+					))}
+				</tbody>
+			</table>
+		</div>
+	);
+};
+
+export { Table };
