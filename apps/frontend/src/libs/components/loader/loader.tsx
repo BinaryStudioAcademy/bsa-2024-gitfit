98bde536026ac2533f2410ed4415b7ed0ace673a
perekotypole <parcersara@gmail.com>
2024-08-19 12:47:11 +0300

    feat: add pulsing animated loader gf-9

--- apps/frontend/src/libs/components/loader/loader.tsx
+++ apps/frontend/src/libs/components/loader/loader.tsx
@@ -2,7 +2,7 @@ import styles from "./styles.module.css";
 
 const Loader = (): JSX.Element => (
 	<div className={styles["loader-wrapper"]}>
-		<span className={styles["loader"]}>Loading</span>
+		<div aria-label="Loading" className={styles["loader"]} role="status" />
 	</div>
 );
 
