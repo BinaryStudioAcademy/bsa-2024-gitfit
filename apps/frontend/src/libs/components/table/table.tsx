92f1df128a8bedbaab2971615015d2ef73ff4522
AnutkaGn <annaartemivna@gmail.com>
2024-08-20 15:15:32 +0300

    fix: remove table container gf-22

--- apps/frontend/src/libs/components/table/table.tsx
+++ apps/frontend/src/libs/components/table/table.tsx
@@ -23,35 +23,33 @@ const Table = <T extends object>({
 	});
 
 	return (
-		<div className={styles["table-container"]}>
-			<table className={styles["table"]}>
-				<thead>
-					{table.getHeaderGroups().map((headerGroup) => (
-						<tr key={headerGroup.id}>
-							{headerGroup.headers.map((header) => (
-								<th key={header.id}>
-									{flexRender(
-										header.column.columnDef.header,
-										header.getContext(),
-									)}
-								</th>
-							))}
-						</tr>
-					))}
-				</thead>
-				<tbody>
-					{table.getRowModel().rows.map((row) => (
-						<tr key={row.id}>
-							{row.getVisibleCells().map((cell) => (
-								<td key={cell.id}>
-									{flexRender(cell.column.columnDef.cell, cell.getContext())}
-								</td>
-							))}
-						</tr>
-					))}
-				</tbody>
-			</table>
-		</div>
+		<table className={styles["table"]}>
+			<thead>
+				{table.getHeaderGroups().map((headerGroup) => (
+					<tr key={headerGroup.id}>
+						{headerGroup.headers.map((header) => (
+							<th key={header.id}>
+								{flexRender(
+									header.column.columnDef.header,
+									header.getContext(),
+								)}
+							</th>
+						))}
+					</tr>
+				))}
+			</thead>
+			<tbody>
+				{table.getRowModel().rows.map((row) => (
+					<tr key={row.id}>
+						{row.getVisibleCells().map((cell) => (
+							<td key={cell.id}>
+								{flexRender(cell.column.columnDef.cell, cell.getContext())}
+							</td>
+						))}
+					</tr>
+				))}
+			</tbody>
+		</table>
 	);
 };
 
