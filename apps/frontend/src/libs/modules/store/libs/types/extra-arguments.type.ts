177bd7b05f96b04a9b33c8ae7f4a2190b7ff9887
perekotypole <parcersara@gmail.com>
2024-08-21 16:07:45 +0300

    fix: storage extra argument type gf-26

--- apps/frontend/src/libs/modules/store/libs/types/extra-arguments.type.ts
+++ apps/frontend/src/libs/modules/store/libs/types/extra-arguments.type.ts
@@ -1,11 +1,11 @@
-import { type storage } from "~/libs/modules/storage/storage.js";
+import { type Storage } from "~/libs/modules/storage/storage.js";
 import { type ToastNotifier } from "~/libs/modules/toast-notifier/toast-notifier.js";
 import { type authApi } from "~/modules/auth/auth.js";
 import { type userApi } from "~/modules/users/users.js";
 
 type ExtraArguments = {
 	authApi: typeof authApi;
-	storage: typeof storage;
+	storage: Storage;
 	toastNotifier: ToastNotifier;
 	userApi: typeof userApi;
 };
