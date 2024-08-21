a0d5a95743cd8e52ff13c38555e2297bc79d7abe
HoroshkoMykhailo <goroshkomihajlo@gmail.com>
2024-08-21 06:30:21 +0300

    fix: delete comments gf-12

--- apps/frontend/src/libs/components/protected-route/protected-route.tsx
+++ apps/frontend/src/libs/components/protected-route/protected-route.tsx
@@ -1,26 +1,25 @@
 import { type ReactNode } from "react";
-// import { Navigate } from "react-router-dom";
+import { Navigate } from "react-router-dom";
 
-// import { AppRoute } from "~/libs/enums/app-route.enum.js";
-// import { useAppSelector } from "~/libs/hooks/hooks.js";
+import { AppRoute } from "~/libs/enums/app-route.enum.js";
+import { useAppSelector } from "~/libs/hooks/hooks.js";
 
 type Properties = {
 	children: ReactNode;
 };
 
 const ProtectedRoute = ({ children }: Properties): JSX.Element => {
-	// const authenticatedUser = useAppSelector(
-	// 	(state) => state.auth.authenticatedUser,
-	// );
+	const authenticatedUser = useAppSelector(
+		(state) => state.auth.authenticatedUser,
+	);
 
-	//const hasAuthenticatedUser = Boolean(authenticatedUser);
+	const hasAuthenticatedUser = Boolean(authenticatedUser);
 
-	// return hasAuthenticatedUser ? (
-	// 	<>{children}</>
-	// ) : (
-	// 	<Navigate replace to={AppRoute.SIGN_IN} />
-	// );
-	return <>{children}</>;
+	return hasAuthenticatedUser ? (
+		<>{children}</>
+	) : (
+		<Navigate replace to={AppRoute.SIGN_IN} />
+	);
 };
 
 export { ProtectedRoute };
