b73b02de9f3104b1806936d3d7dd6ce3f9373940
Mykhailo Pumnia <misha.pumnya@gmail.com>
2024-08-22 11:52:59 +0300

    refactor: clean up header and avatar components gf-11

--- apps/frontend/src/libs/components/avatar/avatar.tsx
+++ apps/frontend/src/libs/components/avatar/avatar.tsx
@@ -1,13 +1,26 @@
 import styles from "./styles.module.css";
 
-interface Properties {
-	name?: string | undefined;
-}
+type Properties = {
+	name?: string;
+};
+
+const FIRST_LETTER_INDEX = 0;
 
 const Avatar = ({ name }: Properties): JSX.Element => {
+	let firstLetter = null;
+
+	if (name && name.length > FIRST_LETTER_INDEX) {
+		firstLetter = name[FIRST_LETTER_INDEX];
+	}
+
+	const hasFirstLetter = Boolean(firstLetter);
+
+	// TODO: render avatar image instead of first letter (in case if image present)
 	return (
 		<div className={styles["avatar"]}>
-			<span className={styles["avatar_letter"]}>{name}</span>
+			{hasFirstLetter && (
+				<span className={styles["avatar_letter"]}>{firstLetter}</span>
+			)}
 		</div>
 	);
 };
--- apps/frontend/src/libs/components/header/header.tsx
+++ apps/frontend/src/libs/components/header/header.tsx
@@ -1,22 +1,19 @@
 import logoSrc from "~/assets/images/logo.svg";
-import {
-	useAppDispatch,
-	useAppSelector,
-	useEffect,
-} from "~/libs/hooks/hooks.js";
-import { actions as userActions } from "~/modules/users/users.js";
+import { useAppSelector } from "~/libs/hooks/hooks.js";
 
 import { Avatar } from "../components.js";
 import styles from "./styles.module.css";
 
 const Header = (): JSX.Element => {
-	const dispatch = useAppDispatch();
-	const users = useAppSelector(({ users }) => users.users); //TODO: Change this to getAuthenticatedUser when is ready
-	const ZERO = 0;
-	const ARRAY_FIRST_INDEX = users.at(ZERO)?.email.at(ZERO); // TODO: Modify this variable when getAuthenticatedUser when is ready
-	useEffect(() => {
-		void dispatch(userActions.loadAll());
-	}, [dispatch]);
+	const authenticatedUser = useAppSelector(
+		({ auth }) => auth.authenticatedUser,
+	);
+
+	if (!authenticatedUser) {
+		return <></>;
+	}
+
+	const userName = authenticatedUser.user.name;
 
 	return (
 		<header className={styles["header"]}>
@@ -24,9 +21,7 @@ const Header = (): JSX.Element => {
 				<img alt="GitFit logo" className={styles["logo-img"]} src={logoSrc} />
 				<span className={styles["logo-text"]}>Logo</span>
 			</div>
-			<div>
-				<Avatar name={ARRAY_FIRST_INDEX} />
-			</div>
+			<Avatar name={userName} />
 		</header>
 	);
 };
