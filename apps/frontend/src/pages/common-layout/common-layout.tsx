fc8c55b00eca536df7d633fdcd9b9f748e3c732d
AnutkaGn <annaartemivna@gmail.com>
2024-08-22 20:58:16 +0300

    feat: create common layout gf-41

---
+++ apps/frontend/src/pages/common-layout/common-layout.tsx
@@ -0,0 +1,27 @@
+import { type ReactNode } from "react";
+
+import { Header } from "~/libs/components/components.js";
+
+import styles from "./styles.module.css";
+
+type CommonLayoutProperties = {
+	children: ReactNode;
+};
+
+const CommonLayout = ({ children }: CommonLayoutProperties): JSX.Element => {
+	return (
+		<div className={styles["container"]}>
+			<Header />
+			<div className={styles["container__sidebar-content"]}>
+				<div
+					style={{ backgroundColor: "white", height: "100vh", width: "280px" }}
+				>
+					{/* add sidebar*/}
+				</div>
+				<main className={styles["content-container"]}>{children}</main>
+			</div>
+		</div>
+	);
+};
+
+export { CommonLayout };
