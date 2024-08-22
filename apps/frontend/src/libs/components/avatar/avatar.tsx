b97c3729a138d61bbea00243ede42c21054e92f6
Mykhailo Pumnia <misha.pumnya@gmail.com>
2024-08-22 14:32:10 +0300

    refactor: clean up avatar component gf-11

--- apps/frontend/src/libs/components/avatar/avatar.tsx
+++ apps/frontend/src/libs/components/avatar/avatar.tsx
@@ -1,26 +1,17 @@
 import styles from "./styles.module.css";
 
 type Properties = {
-	name?: string;
+	name: string;
 };
 
 const FIRST_LETTER_INDEX = 0;
 
 const Avatar = ({ name }: Properties): JSX.Element => {
-	let firstLetter = null;
+	const firstLetter = name[FIRST_LETTER_INDEX];
 
-	if (name && name.length > FIRST_LETTER_INDEX) {
-		firstLetter = name[FIRST_LETTER_INDEX];
-	}
-
-	const hasFirstLetter = Boolean(firstLetter);
-
-	// TODO: render avatar image instead of first letter (in case if image present)
 	return (
 		<div className={styles["avatar"]}>
-			{hasFirstLetter && (
-				<span className={styles["avatar_letter"]}>{firstLetter}</span>
-			)}
+			<span className={styles["avatar_letter"]}>{firstLetter}</span>
 		</div>
 	);
 };
