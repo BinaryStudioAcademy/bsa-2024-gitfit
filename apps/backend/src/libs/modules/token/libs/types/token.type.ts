b9f82b8f411500dd73692d53ab7c629173668636
sandrvvu <sandrvvu@gmail.com>
2024-08-22 14:00:40 +0300

    fix: token type missed decode token gf-13

--- apps/backend/src/libs/modules/token/libs/types/token.type.ts
+++ apps/backend/src/libs/modules/token/libs/types/token.type.ts
@@ -2,6 +2,7 @@ import { type JWTPayload } from "jose";
 
 type Token = {
 	createToken: (payload: JWTPayload) => Promise<string>;
+	decodeToken: (token: string) => JWTPayload;
 	verifyToken: (token: string) => Promise<JWTPayload>;
 };
 
