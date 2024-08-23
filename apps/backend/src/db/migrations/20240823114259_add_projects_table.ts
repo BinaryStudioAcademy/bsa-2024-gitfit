1716bf76e6be6705186266aff8bc190b36e3bfb5
perekotypole <parcersara@gmail.com>
2024-08-23 14:51:06 +0300

    feat: add project table gf-57

---
+++ apps/backend/src/db/migrations/20240823114259_add_projects_table.ts
@@ -0,0 +1,35 @@
+import { type Knex } from "knex";
+
+const TABLE_NAME = "projects";
+
+const ColumnName = {
+	API_KEY: "api_key",
+	CREATED_AT: "created_at",
+	DESCRIPTION: "description",
+	ID: "id",
+	NAME: "name",
+	UPDATED_AT: "updated_at",
+} as const;
+
+function up(knex: Knex): Promise<void> {
+	return knex.schema.createTable(TABLE_NAME, (table) => {
+		table.increments(ColumnName.ID).primary();
+		table.string(ColumnName.NAME).unique().notNullable();
+		table.text(ColumnName.DESCRIPTION);
+		table.string(ColumnName.API_KEY).unique().notNullable();
+		table
+			.dateTime(ColumnName.CREATED_AT)
+			.notNullable()
+			.defaultTo(knex.fn.now());
+		table
+			.dateTime(ColumnName.UPDATED_AT)
+			.notNullable()
+			.defaultTo(knex.fn.now());
+	});
+}
+
+function down(knex: Knex): Promise<void> {
+	return knex.schema.dropTableIfExists(TABLE_NAME);
+}
+
+export { down, up };
