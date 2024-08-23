0c11725fa257b1847bf1f496951fdcae02d637eb
kolibri753 <kolibri753@gmail.com>
2024-08-23 15:44:01 +0300

    feat: add relatable to user_groups table names gf-53

--- apps/backend/src/libs/modules/database/libs/enums/database-table-name.enum.ts
+++ apps/backend/src/libs/modules/database/libs/enums/database-table-name.enum.ts
@@ -1,6 +1,9 @@
 const DatabaseTableName = {
 	MIGRATIONS: "migrations",
+	USER_GROUPS: "user_groups",
+	USER_GROUPS_TO_PERMISSIONS: "user_groups_to_permissions",
 	USERS: "users",
+	USERS_TO_USER_GROUPS: "users_to_user_groups",
 } as const;
 
 export { DatabaseTableName };
