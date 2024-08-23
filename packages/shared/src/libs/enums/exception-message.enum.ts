8e8de81077275142027e05e1dd6133e42faf80db
Oleksandra Sydorenko <105361812+sandrvvu@users.noreply.github.com>
2024-08-23 10:45:28 +0300

    style: adding a dot to the end gf-13
    Co-authored-by: Yelyzaveta Veis <64521658+liza-veis@users.noreply.github.com>
    Signed-off-by: Oleksandra Sydorenko <105361812+sandrvvu@users.noreply.github.com>

--- packages/shared/src/libs/enums/exception-message.enum.ts
+++ packages/shared/src/libs/enums/exception-message.enum.ts
@@ -1,7 +1,7 @@
 const ExceptionMessage = {
 	EMAIL_USED: "Email address is already in use.",
 	INVALID_CREDENTIALS: "Invalid credentials.",
-	INVALID_TOKEN: "Invalid token",
+	INVALID_TOKEN: "Invalid token.",
 	NO_TOKEN_PROVIDED: "No token provided.",
 	TOKEN_EXPIRED: "Token is expired.",
 	USER_NOT_FOUND: "User not found.",
