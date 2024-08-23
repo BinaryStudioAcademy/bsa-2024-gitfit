4a0b23d86e5641c1cf9eff8bc3a7d75e3d993c79
Joel Strid <joelstridgg@gmail.com>
2024-08-23 21:26:45 +0200

    fix: cleaned up comments in code gf-21

--- apps/frontend/src/libs/hooks/use-handle-click-outside/use-handle-click-outside-hook.ts
+++ apps/frontend/src/libs/hooks/use-handle-click-outside/use-handle-click-outside-hook.ts
@@ -1,12 +1,10 @@
 import { type RefObject, useEffect } from "react";
 
-// The hook itself doesn't return anything (void return type)
 function useHandleClickOutside<T extends HTMLElement>(
 	reference: RefObject<T>,
 	onClick: () => void,
 ): void {
 	useEffect(() => {
-		// handleClickOutside function, which also has a void return type
 		function handleClickOutside(event: MouseEvent): void {
 			if (
 				reference.current &&
@@ -16,10 +14,8 @@ function useHandleClickOutside<T extends HTMLElement>(
 			}
 		}
 
-		// Add event listener to document
 		document.addEventListener("mousedown", handleClickOutside);
 
-		// The cleanup function of useEffect also has a void return type
 		return (): void => {
 			document.removeEventListener("mousedown", handleClickOutside);
 		};
