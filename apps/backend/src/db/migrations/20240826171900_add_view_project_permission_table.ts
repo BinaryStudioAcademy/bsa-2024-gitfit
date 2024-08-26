79157c000b4238ffa4dc6e456534bc5acc079368
sandrvvu <sandrvvu@gmail.com>
2024-08-26 20:59:07 +0300

    feat: add view project permission gf-71

---
+++ apps/backend/src/db/migrations/20240826171900_add_view_project_permission_table.ts
@@ -0,0 +1,24 @@
+import { type Knex } from "knex";
+
+const TABLE_NAME = "permissions";
+
+const PermissionName = {
+	VIEW_PROJECT: "View Project",
+} as const;
+
+function up(knex: Knex): Promise<void> {
+	return knex(TABLE_NAME).insert({
+		key: "view_project",
+		name: PermissionName.VIEW_PROJECT,
+	});
+}
+
+function down(knex: Knex): Promise<void> {
+	return knex(TABLE_NAME)
+		.where({
+			name: PermissionName.VIEW_PROJECT,
+		})
+		.del();
+}
+
+export { down, up };
