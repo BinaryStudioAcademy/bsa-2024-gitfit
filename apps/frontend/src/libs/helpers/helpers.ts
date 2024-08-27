fed233d4ef5f4b80a9fb77f4e9d3f0d9e831d83b
AnutkaGn <annaartemivna@gmail.com>
2024-08-27 19:24:14 +0300

    fix: change the file name gf-41

--- apps/frontend/src/libs/helpers/helpers.ts
+++ apps/frontend/src/libs/helpers/helpers.ts
@@ -1,3 +1,3 @@
-export { getUserColumns } from "./get-user-columns/get-user-columns.js";
+export { getUserColumns } from "./get-user-columns/get-user-columns.helper.js";
 export { getValidClassNames } from "./get-valid-classes/get-valid-classes.helper.js";
 export { configureString, formatDate } from "@git-fit/shared";
