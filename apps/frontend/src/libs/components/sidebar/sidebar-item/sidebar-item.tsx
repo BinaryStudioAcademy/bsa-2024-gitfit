af360514c963de7cc905567818fce81c3f42cf7e
HoroshkoMykhailo <goroshkomihajlo@gmail.com>
2024-08-30 20:35:58 +0300

    fix: get class name positioning gf-118

--- apps/frontend/src/libs/components/sidebar/sidebar-item/sidebar-item.tsx
+++ apps/frontend/src/libs/components/sidebar/sidebar-item/sidebar-item.tsx
@@ -1,17 +1,21 @@
 import { Icon, NavLink } from "~/libs/components/components.js";
 import { getValidClassNames } from "~/libs/helpers/helpers.js";
+import { useCallback } from "~/libs/hooks/hooks.js";
 import { type NavigationItem } from "~/libs/types/types.js";
 
 import styles from "./styles.module.css";
 
-const getClassName = ({ isActive }: { isActive: boolean }): string => {
-	return getValidClassNames(
-		styles["navigation-link"],
-		isActive && styles["active"],
-	);
-};
-
 const SidebarItem = ({ href, icon, label }: NavigationItem): JSX.Element => {
+	const getClassName = useCallback(
+		({ isActive }: { isActive: boolean }): string => {
+			return getValidClassNames(
+				styles["navigation-link"],
+				isActive && styles["active"],
+			);
+		},
+		[],
+	);
+
 	return (
 		<li className={styles["sidebar-item"]}>
 			<NavLink className={getClassName} to={href}>
