1c5ea8884a9e46093b3329748c6f8b5f7a6ef1c3
HoroshkoMykhailo <goroshkomihajlo@gmail.com>
2024-08-30 09:23:49 +0300

    fix: sidebar item highlight gf-118

--- apps/frontend/src/libs/components/sidebar/sidebar-item/sidebar-item.tsx
+++ apps/frontend/src/libs/components/sidebar/sidebar-item/sidebar-item.tsx
@@ -1,19 +1,29 @@
-import { NavLink } from "react-router-dom";
+import { NavLink, useLocation } from "react-router-dom";
 
 import { Icon } from "~/libs/components/components.js";
 import { type NavigationItem } from "~/libs/types/types.js";
 
 import styles from "./styles.module.css";
 
-const SidebarItem = ({ href, icon, label }: NavigationItem): JSX.Element => (
-	<li className={styles["sidebar-item"]}>
-		<NavLink className={styles["navigation-link"] ?? ""} to={href}>
-			<span className={styles["navigation-icon"]}>
-				<Icon height={20} name={icon} width={20} />
-			</span>
-			<label className={styles["navigation-item-label"]}>{label}</label>
-		</NavLink>
-	</li>
-);
+const SidebarItem = ({ href, icon, label }: NavigationItem): JSX.Element => {
+	const location = useLocation();
+	const currentPath = location.pathname;
+
+	const isActive = currentPath === href;
+
+	return (
+		<li className={styles["sidebar-item"]}>
+			<NavLink
+				className={`${styles["navigation-link"] ?? ""} ${isActive ? (styles["active"] ?? "") : ""}`}
+				to={href}
+			>
+				<span className={styles["navigation-icon"]}>
+					<Icon height={20} name={icon} width={20} />
+				</span>
+				<label className={styles["navigation-item-label"]}>{label}</label>
+			</NavLink>
+		</li>
+	);
+};
 
 export { SidebarItem };
