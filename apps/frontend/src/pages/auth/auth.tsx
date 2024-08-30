918889d3e502af3cfc4880d4beaf3f2c20b6da25
kolibri753 <kolibri753@gmail.com>
2024-08-30 13:24:28 +0300

    refactor: move and rename placeholder logo image gf-121

--- apps/frontend/src/pages/auth/auth.tsx
+++ apps/frontend/src/pages/auth/auth.tsx
@@ -1,6 +1,6 @@
 import { useNavigate } from "react-router-dom";
 
-import logoSrc from "~/assets/images/icons/logo.svg";
+import logoSrc from "~/assets/images/logo-default.svg";
 import { AppRoute } from "~/libs/enums/enums.js";
 import {
 	useAppDispatch,
