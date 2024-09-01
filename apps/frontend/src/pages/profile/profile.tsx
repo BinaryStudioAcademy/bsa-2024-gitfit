e550b9580c09ec2c8db11f15bb1a209c1d6567e7
perekotypole <parcersara@gmail.com>
2024-09-01 23:15:56 +0300

    fix: remove unnecessary checks gf-89

--- apps/frontend/src/pages/profile/profile.tsx
+++ apps/frontend/src/pages/profile/profile.tsx
@@ -1,32 +1,19 @@
-import { Loader, PageLayout } from "~/libs/components/components.js";
-import { DataStatus } from "~/libs/enums/data-status.enum.js";
+import { PageLayout } from "~/libs/components/components.js";
 import { useAppSelector } from "~/libs/hooks/hooks.js";
+import { type UserAuthResponseDto } from "~/modules/users/users.js";
 
 import { EditUserForm } from "./components/components.js";
 import styles from "./styles.module.css";
 
 const Profile = (): JSX.Element => {
-	const { authenticatedUser, dataStatus } = useAppSelector(({ auth }) => auth);
-
-	if (!authenticatedUser) {
-		return <></>;
-	}
-
-	const isLoading =
-		dataStatus === DataStatus.PENDING || dataStatus === DataStatus.IDLE;
+	const { authenticatedUser } = useAppSelector(({ auth }) => auth);
 
 	return (
 		<PageLayout>
 			<div className={styles["profile-content"]}>
-				<h1 className={styles["label"]}>Profile</h1>
+				<h1 className={styles["title"]}>Profile</h1>
 
-				{isLoading ? (
-					<div className={styles["profile-loader"]}>
-						<Loader />
-					</div>
-				) : (
-					<EditUserForm user={authenticatedUser} />
-				)}
+				<EditUserForm user={authenticatedUser as UserAuthResponseDto} />
 			</div>
 		</PageLayout>
 	);
