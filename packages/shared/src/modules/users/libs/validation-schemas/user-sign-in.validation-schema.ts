2df6ef4c192ab89277b2e00371a99645767672d6
kolibri753 <kolibri753@gmail.com>
2024-09-02 09:35:21 +0300

    feat: add password validation rules gf-134

--- packages/shared/src/modules/users/libs/validation-schemas/user-sign-in.validation-schema.ts
+++ packages/shared/src/modules/users/libs/validation-schemas/user-sign-in.validation-schema.ts
@@ -19,6 +19,21 @@ const userSignIn: z.ZodType<UserSignInRequestDto> = z
 			.trim()
 			.min(UserValidationRule.PASSWORD_MINIMUM_LENGTH, {
 				message: UserValidationMessage.PASSWORD_MINIMUM_LENGTH,
+			})
+			.max(UserValidationRule.PASSWORD_MAXIMUM_LENGTH, {
+				message: UserValidationMessage.PASSWORD_TOO_LONG,
+			})
+			.regex(UserValidationRule.PASSWORD_UPPERCASE_PATTERN, {
+				message: UserValidationMessage.PASSWORD_UPPERCASE_REQUIRED,
+			})
+			.regex(UserValidationRule.PASSWORD_LOWERCASE_PATTERN, {
+				message: UserValidationMessage.PASSWORD_LOWERCASE_REQUIRED,
+			})
+			.regex(UserValidationRule.PASSWORD_NUMBER_PATTERN, {
+				message: UserValidationMessage.PASSWORD_NUMBER_REQUIRED,
+			})
+			.regex(UserValidationRule.PASSWORD_SPECIAL_CHAR_PATTERN, {
+				message: UserValidationMessage.PASSWORD_SPECIAL_CHAR_REQUIRED,
 			}),
 	})
 	.required();
