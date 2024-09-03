915a65d8f0204a42f394f92487f6f898c3307bf4
Vitalii Stefaniv <deosplayt@gmail.com>
2024-09-03 18:19:19 +0300

    feat: added scroll to the table component gf-149

--- apps/frontend/src/libs/components/table/table.tsx
+++ apps/frontend/src/libs/components/table/table.tsx
@@ -24,33 +24,35 @@ const Table = <T extends object>({
 	});
 
 	return (
-		<table className={styles["table"]}>
-			<thead>
-				{table.getHeaderGroups().map((headerGroup) => (
-					<tr key={headerGroup.id}>
-						{headerGroup.headers.map((header) => (
-							<th key={header.id}>
-								{flexRender(
-									header.column.columnDef.header,
-									header.getContext(),
-								)}
-							</th>
-						))}
-					</tr>
-				))}
-			</thead>
-			<tbody>
-				{table.getRowModel().rows.map((row) => (
-					<tr key={row.id}>
-						{row.getVisibleCells().map((cell) => (
-							<td key={cell.id}>
-								{flexRender(cell.column.columnDef.cell, cell.getContext())}
-							</td>
-						))}
-					</tr>
-				))}
-			</tbody>
-		</table>
+		<div className={styles["table-container"]}>
+			<table className={styles["table"]}>
+				<thead>
+					{table.getHeaderGroups().map((headerGroup) => (
+						<tr key={headerGroup.id}>
+							{headerGroup.headers.map((header) => (
+								<th key={header.id}>
+									{flexRender(
+										header.column.columnDef.header,
+										header.getContext(),
+									)}
+								</th>
+							))}
+						</tr>
+					))}
+				</thead>
+				<tbody>
+					{table.getRowModel().rows.map((row) => (
+						<tr key={row.id}>
+							{row.getVisibleCells().map((cell) => (
+								<td key={cell.id}>
+									{flexRender(cell.column.columnDef.cell, cell.getContext())}
+								</td>
+							))}
+						</tr>
+					))}
+				</tbody>
+			</table>
+		</div>
 	);
 };
 
