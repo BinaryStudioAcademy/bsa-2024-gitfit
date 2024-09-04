ced66a8272433da7562c4cc32e2755f904ae6297
HoroshkoMykhailo <goroshkomihajlo@gmail.com>
2024-09-04 13:37:16 +0300

    feat: add confirmation modal gf-146

--- apps/frontend/src/pages/profile/components/delete-account/delete-account.tsx
+++ apps/frontend/src/pages/profile/components/delete-account/delete-account.tsx
@@ -1,4 +1,5 @@
-import { Button } from "~/libs/components/components.js";
+import { Button, ConfirmationModal } from "~/libs/components/components.js";
+import { useCallback, useModal } from "~/libs/hooks/hooks.js";
 import { type UserAuthResponseDto } from "~/modules/users/users.js";
 
 import styles from "./styles.module.css";
@@ -8,11 +9,33 @@ type Properties = {
 };
 
 const DeleteAccount = ({ user }: Properties): JSX.Element => {
+	const { isModalOpened, onModalClose, onModalOpen } = useModal();
+
+	const handleDeleteClick = useCallback((): void => {
+		onModalOpen();
+	}, [onModalOpen]);
+
+	const handleDeleteConfirm = useCallback(() => {
+		onModalClose();
+	}, [onModalClose]);
+
 	return (
 		<div className={styles["profile-delete"]}>
 			<h2 className={styles["delete-title"]}>Delete account {user.name}</h2>
 			<p className={styles["delete-text"]}>This action cannot be undone.</p>
-			<Button label="Delete Your Account" variant="danger" />
+			<Button
+				label="Delete Your Account"
+				onClick={handleDeleteClick}
+				variant="danger"
+			/>
+			<ConfirmationModal
+				confirmationText="The account will be marked as deleted. The account won`t be accessible, but it can be restored if needed. Do you want to continue?"
+				confirmLabel="Yes, Delete it"
+				isModalOpened={isModalOpened}
+				onConfirm={handleDeleteConfirm}
+				onModalClose={onModalClose}
+				title="Are you sure?"
+			/>
 		</div>
 	);
 };
