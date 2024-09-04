3d8b4ade745f2efb2180243a4acd73da035069ce
AnutkaGn <annaartemivna@gmail.com>
2024-09-04 23:48:28 +0300

    fix: added error message gf-84

--- apps/frontend/src/libs/components/protected-route/protected-route.tsx
+++ apps/frontend/src/libs/components/protected-route/protected-route.tsx
@@ -1,8 +1,13 @@
 import { Navigate } from "react-router-dom";
 
 import { Loader } from "~/libs/components/components.js";
-import { AppRoute, DataStatus } from "~/libs/enums/enums.js";
-import { useAppSelector } from "~/libs/hooks/hooks.js";
+import {
+	AppRoute,
+	DataStatus,
+	NotificationMessage,
+} from "~/libs/enums/enums.js";
+import { useAppSelector, useRef } from "~/libs/hooks/hooks.js";
+import { toastNotifier } from "~/libs/modules/toast-notifier/toast-notifier.js";
 import { type PermissionGetAllItemResponseDto } from "~/modules/permissions/libs/types/types.js";
 
 import styles from "./styles.module.css";
@@ -18,6 +23,8 @@ const ProtectedRoute = ({
 }: Properties): JSX.Element => {
 	const { authenticatedUser, dataStatus } = useAppSelector(({ auth }) => auth);
 	const { permissions } = useAppSelector(({ permissions }) => permissions);
+
+	const hasShownError = useRef(false);
 
 	const hasAuthenticatedUser = Boolean(authenticatedUser);
 
@@ -42,7 +49,12 @@ const ProtectedRoute = ({
 	}
 
 	if (!hasRequiredPermission) {
-		return <Navigate replace to={AppRoute.ANY} />;
+		if (!hasShownError.current) {
+			toastNotifier.showError(NotificationMessage.PERMISSIONS_REQUIRED);
+			hasShownError.current = true;
+		}
+
+		return <Navigate replace to={AppRoute.ROOT} />;
 	}
 
 	if (!hasAuthenticatedUser) {
--- apps/frontend/src/libs/enums/notification-message.enum.ts
+++ apps/frontend/src/libs/enums/notification-message.enum.ts
@@ -1,4 +1,5 @@
 const NotificationMessage = {
+	PERMISSIONS_REQUIRED: "Access to the page is forbidden",
 	PROJECT_CREATE_SUCCESS: "Project was successfully created",
 	PROJECT_UPDATE_SUCCESS: "Project was successfully updated.",
 	SUCCESS_PROFILE_UPDATE: "Successfully updated profile information.",
