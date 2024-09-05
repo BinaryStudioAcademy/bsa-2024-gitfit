76547f4cb6b441974b90b0da7ba57d74c9bc9058
Pumnia Mykhailo <74021315+GvoFor@users.noreply.github.com>
2024-09-05 17:50:06 +0300

    feat: Add an ability to delete a project gf-138 (#168)
    * feat: add an ability to delete project on backend gf-138
    
    * fix: throw error when project to delete not found gf-138
    
    * feat: add trash-bin icon gf-138
    
    * feat: add delete button to project menu gf-138
    
    * feat: add ConfirmationModal and link it with delete button gf-138
    
    * feat: complete the ability to delete a project gf-138
    
    * fix: remove unnecessary content-type headers from api requests gf-138
    
    * fix: optimize useSelector request and fix warning with it gf-138
    
    * refactor: rid of redudent constant gf-138
    
    * refactor: rid of redudent props in confirmation modal gf-138
    
    * fix: incorrect swagger comment gf-138
    
    ---------
    
    Co-authored-by: Yelyzaveta Veis <64521658+liza-veis@users.noreply.github.com>

--- apps/frontend/src/modules/projects/slices/projects.ts
+++ apps/frontend/src/modules/projects/slices/projects.ts
@@ -1,9 +1,10 @@
-import { create, getById, loadAll, patch } from "./actions.js";
+import { create, deleteById, getById, loadAll, patch } from "./actions.js";
 import { actions } from "./project.slice.js";
 
 const allActions = {
 	...actions,
 	create,
+	deleteById,
 	getById,
 	loadAll,
 	patch,
