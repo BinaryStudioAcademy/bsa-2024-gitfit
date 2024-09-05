595d266c4db1924807eea317a731455c0bbb5319
Joel Strid <joelstridgg@gmail.com>
2024-09-05 19:41:53 +0200

    fix: adjusted input to include icon  gf-200

--- apps/frontend/src/libs/components/search/search.tsx
+++ apps/frontend/src/libs/components/search/search.tsx
@@ -1,4 +1,4 @@
-import { Input } from "~/libs/components/components.js";
+import { Icon, Input } from "~/libs/components/components.js";
 import { initDebounce } from "~/libs/helpers/helpers.js";
 import { useAppForm, useEffect, useFormWatch } from "~/libs/hooks/hooks.js";
 
@@ -16,19 +16,22 @@ const Search = ({
 	label,
 	onChange,
 }: Properties): JSX.Element => {
+	// Initialize searchQueryParams hook
 	const { searchQuery, updateSearchParams } = useSearchQueryParameters("");
 
+	// Set up form control
 	const { control, errors } = useAppForm({
-		defaultValues: { search: searchQuery },
+		defaultValues: { search: searchQuery }, // Initialize with query parameter value
 		mode: "onChange",
 	});
 
+	// Watch for changes in the search input
 	const value = useFormWatch({ control, name: "search" });
 
 	useEffect(() => {
 		const debouncedOnChange = initDebounce(() => {
 			onChange(value);
-			updateSearchParams(value);
+			updateSearchParams(value); // Update the query parameters on input change
 		}, SEARCH_TIMEOUT);
 
 		debouncedOnChange(value);
@@ -44,6 +47,7 @@ const Search = ({
 			errors={errors}
 			isLabelHidden={isLabelHidden}
 			label={label}
+			leftIcon={<Icon height={20} name="search" width={20} />} // Icon reintroduced here
 			name="search"
 			placeholder="Enter project name"
 			type="search"
