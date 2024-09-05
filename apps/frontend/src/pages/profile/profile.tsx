1911604c4836705ecbbd20961d8ed492e58374a2
HoroshkoMykhailo <goroshkomihajlo@gmail.com>
2024-09-05 18:29:07 +0300

    fix: add type casting in profile gf-146

--- apps/frontend/src/pages/profile/profile.tsx
+++ apps/frontend/src/pages/profile/profile.tsx
@@ -7,18 +7,17 @@ import styles from "./styles.module.css";
 
 const Profile = (): JSX.Element => {
 	const { authenticatedUser } = useAppSelector(({ auth }) => auth);
+	const user = authenticatedUser as UserAuthResponseDto;
 
 	return (
 		<PageLayout>
 			<div className={styles["profile-layout"]}>
 				<div className={styles["profile-info"]}>
 					<h1 className={styles["title"]}>Profile</h1>
-					<EditUserForm user={authenticatedUser as UserAuthResponseDto} />
+					<EditUserForm user={user} />
 				</div>
 				<div className={styles["divider"]} />
-				{authenticatedUser?.id && (
-					<DeleteAccount userId={authenticatedUser.id} />
-				)}
+				<DeleteAccount userId={user.id} />
 			</div>
 		</PageLayout>
 	);
