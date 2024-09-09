11ac87ddc6a831afd54bb2768278d92417223a85
kolibri753 <kolibri753@gmail.com>
2024-09-09 17:07:11 +0300

    refactor: small fixes gf-202

--- apps/frontend/src/libs/components/avatar/avatar.tsx
+++ apps/frontend/src/libs/components/avatar/avatar.tsx
@@ -3,24 +3,25 @@ import { getValidClassNames } from "~/libs/helpers/helpers.js";
 import styles from "./styles.module.css";
 
 type Properties = {
-	imageSrc?: string;
+	imageUrl?: string;
 	name: string;
 };
 
 const FIRST_LETTER_INDEX = 0;
 
-const Avatar = ({ imageSrc, name }: Properties): JSX.Element => {
+const Avatar = ({ imageUrl, name }: Properties): JSX.Element => {
 	const firstLetter = name[FIRST_LETTER_INDEX];
+	const hasImage = Boolean(imageUrl);
 
 	return (
 		<div
 			className={getValidClassNames(
 				styles["avatar"],
-				!imageSrc && styles["no-image"],
+				!imageUrl && styles["no-image"],
 			)}
 		>
-			{imageSrc ? (
-				<img alt={name} className={styles["avatar-image"]} src={imageSrc} />
+			{hasImage ? (
+				<img alt={name} className={styles["avatar-image"]} src={imageUrl} />
 			) : (
 				<span className={styles["avatar-letter"]}>{firstLetter}</span>
 			)}
