0ac3a14f24eb26a43a7515fe5c4319e082bd57c6
Vitalii Stefaniv <deosplayt@gmail.com>
2024-09-09 21:37:29 +0300

    refactor: changed formatDate helper gf-182

--- packages/shared/src/libs/helpers/format-date/format-date.helper.ts
+++ packages/shared/src/libs/helpers/format-date/format-date.helper.ts
@@ -1,8 +1,11 @@
 import { format } from "date-fns";
 
-type DateFormat = "d MMM yyyy HH:mm";
+type DateFormat = "d MMM yyyy HH:mm" | "MMM d, yyyy";
 
-const formatDate = (date: Date, dateFormat: DateFormat): string => {
+const formatDate = (
+	date: Date,
+	dateFormat: DateFormat = "d MMM yyyy HH:mm",
+): string => {
 	return format(date, dateFormat);
 };
 
