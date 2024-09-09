02018f7278cfae25da75da08e36c4a3b1d933fe0
kolibri753 <kolibri753@gmail.com>
2024-09-09 11:09:07 +0300

    refactor: update avatar to have a border when img is not provided gf-202

--- apps/frontend/src/libs/components/avatar/avatar.tsx
+++ apps/frontend/src/libs/components/avatar/avatar.tsx
@@ -1,17 +1,29 @@
+import { getValidClassNames } from "~/libs/helpers/helpers.js";
+
 import styles from "./styles.module.css";
 
 type Properties = {
+	imageSrc?: string;
 	name: string;
 };
 
 const FIRST_LETTER_INDEX = 0;
 
-const Avatar = ({ name }: Properties): JSX.Element => {
+const Avatar = ({ imageSrc, name }: Properties): JSX.Element => {
 	const firstLetter = name[FIRST_LETTER_INDEX];
 
 	return (
-		<div className={styles["avatar"]}>
-			<span className={styles["avatar_letter"]}>{firstLetter}</span>
+		<div
+			className={getValidClassNames(
+				styles["avatar"],
+				!imageSrc && styles["no-image"],
+			)}
+		>
+			{imageSrc ? (
+				<img alt={name} className={styles["avatar-image"]} src={imageSrc} />
+			) : (
+				<span className={styles["avatar-letter"]}>{firstLetter}</span>
+			)}
 		</div>
 	);
 };
