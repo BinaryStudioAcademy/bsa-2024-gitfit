a653cba9ae237675513e4e6e37cc3109ee3969f5
sandrvvu <sandrvvu@gmail.com>
2024-09-10 19:40:39 +0300

    fix: exceptions message format gf-230

--- packages/shared/src/libs/enums/exception-message.enum.ts
+++ packages/shared/src/libs/enums/exception-message.enum.ts
@@ -1,7 +1,6 @@
 const ExceptionMessage = {
+	ACTIVITY_LOG_CREATE_FAILED: "Failed to save activity logs.",
 	CONTRIBUTOR_NOT_FOUND: "Contributor not found.",
-	CREATE_ACTIVITY_LOG_FAILED: "Failed to save activity logs.",
-	CREATE_PROJECT_GROUP_FAILED: "Failed to create project group.",
 	EMAIL_USED: "Email address is already in use.",
 	GIT_EMAIL_NOT_FOUND: "Git email not found.",
 	GROUP_NAME_USED: "Group name is already in use.",
@@ -10,6 +9,7 @@ const ExceptionMessage = {
 	INVALID_TOKEN: "Invalid token.",
 	INVALID_TOKEN_NO_USER_ID: "Token is invalid: userId missing.",
 	NO_TOKEN_PROVIDED: "No token provided.",
+	PROJECT_GROUP_CREATE_FAILED: "Failed to create project group.",
 	PROJECT_GROUP_NAME_USED: "Project group name is already in use.",
 	PROJECT_NAME_USED: "Project name is already in use.",
 	PROJECT_NOT_FOUND: "Project not found.",
