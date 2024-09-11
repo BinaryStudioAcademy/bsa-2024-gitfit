27f10b7a9b0454156d2ea936516ec9bf9bd34b2f
Serhii Serhiienko <sirserg05@gmail.com>
2024-09-11 10:41:00 +0100

    fix: fix inconsistent alignment in create group modal gf-265 (#268)
    * fix: fix inconsistent alignment in create group modal gf-265
    
    * fix: refactor table component selectable row styles gf-265

--- apps/frontend/src/libs/components/table/table.tsx
+++ apps/frontend/src/libs/components/table/table.tsx
@@ -5,6 +5,7 @@ import {
 } from "@tanstack/react-table";
 
 import { EMPTY_LENGTH } from "~/libs/constants/constants.js";
+import { getValidClassNames } from "~/libs/helpers/helpers.js";
 import { type TableColumn } from "~/libs/types/types.js";
 
 import { SelectRowCell } from "./libs/components/components.js";
@@ -49,7 +50,14 @@ const Table = <T extends object>({
 				<thead className={styles["table-head"]}>
 					{table.getHeaderGroups().map((headerGroup) => (
 						<tr className={styles["table-row"]} key={headerGroup.id}>
-							{isRowSelectable && <th className={styles["table-header"]} />}
+							{isRowSelectable && (
+								<th
+									className={getValidClassNames(
+										styles["table-header"],
+										styles["cell-min-content"],
+									)}
+								/>
+							)}
 							{headerGroup.headers.map((header) => (
 								<th
 									className={styles["table-header"]}
@@ -72,7 +80,12 @@ const Table = <T extends object>({
 						table.getRowModel().rows.map((row) => (
 							<tr className={styles["table-row"]} key={row.id}>
 								{isRowSelectable && (
-									<td className={styles["table-data"]}>
+									<td
+										className={getValidClassNames(
+											styles["table-data"],
+											styles["cell-min-content"],
+										)}
+									>
 										<SelectRowCell
 											id={getRowId(row.original)}
 											isChecked={selectedRowIds.includes(
