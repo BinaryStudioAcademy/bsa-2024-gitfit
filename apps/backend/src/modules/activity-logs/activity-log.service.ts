74b5c5be204f9a000fc5537f88f556f7241be3e9
Oleksandra Sydorenko <105361812+sandrvvu@users.noreply.github.com>
2024-09-16 09:32:49 +0300

    feat: add an ability to save and retrieve activity logs gf-181 (#221)
    * feat: add contributors table gf-181
    
    * feat: add git emails table gf-181
    
    * feat: add activity logs table gf-181
    
    * feat: activity log module template gf-181
    
    * feat: attachedcontributor data to get method response gf-181
    
    * fix: exceptions message format gf-230
    
    * fix: exceptions message format gf-181
    
    * fix: add unsigned for commits number gf-181
    
    * feat: separating contributors and git emails modules gf-181
    
    * feat: integrate api keys module into activity logs gf-181
    
    * refactor: gf-181
    
    * fix: update db schema gf-181
    
    * feat: add req methods check in auth plugin gf-181
    
    * refactor: modules and auth plugin gf-181
    
    * fix: rename file gf-181
    
    * fix: add contributor id to git email entity gf-181
    
    * refactor: add activity log create method gf-181
    
    * fix: type naming and location gf-181

---
+++ apps/backend/src/modules/activity-logs/activity-log.service.ts
@@ -0,0 +1,124 @@
+import { type Service } from "~/libs/types/types.js";
+import { type ContributorService } from "~/modules/contributors/contributors.js";
+import { type GitEmailService } from "~/modules/git-emails/git-emails.js";
+import { type ProjectApiKeyService } from "~/modules/project-api-keys/project-api-keys.js";
+
+import { ActivityLogEntity } from "./activity-log.entity.js";
+import { type ActivityLogRepository } from "./activity-log.repository.js";
+import {
+	type ActivityLogCreateItemResponseDto,
+	type ActivityLogCreateRequestDto,
+	type ActivityLogGetAllResponseDto,
+} from "./libs/types/types.js";
+
+type Constructor = {
+	activityLogRepository: ActivityLogRepository;
+	contributorService: ContributorService;
+	gitEmailService: GitEmailService;
+	projectApiKeyService: ProjectApiKeyService;
+};
+
+class ActivityLogService implements Service {
+	private activityLogRepository: ActivityLogRepository;
+	private contributorService: ContributorService;
+	private gitEmailService: GitEmailService;
+	private projectApiKeyService: ProjectApiKeyService;
+
+	public constructor({
+		activityLogRepository,
+		contributorService,
+		gitEmailService,
+		projectApiKeyService,
+	}: Constructor) {
+		this.activityLogRepository = activityLogRepository;
+		this.contributorService = contributorService;
+		this.gitEmailService = gitEmailService;
+		this.projectApiKeyService = projectApiKeyService;
+	}
+
+	private async createActivityLog({
+		date,
+		logItem,
+		projectId,
+		userId,
+	}: ActivityLogCreateItemResponseDto): Promise<ActivityLogEntity> {
+		const { authorEmail, authorName, commitsNumber } = logItem;
+
+		let gitEmail = await this.gitEmailService.findByEmail(authorEmail);
+
+		if (!gitEmail) {
+			const contributor = await this.contributorService.create({
+				name: authorName,
+			});
+
+			gitEmail = await this.gitEmailService.create({
+				contributorId: contributor.id,
+				email: authorEmail,
+			});
+		}
+
+		return await this.activityLogRepository.create(
+			ActivityLogEntity.initializeNew({
+				commitsNumber,
+				createdByUser: { id: userId },
+				date,
+				gitEmail: { contributor: gitEmail.contributor, id: gitEmail.id },
+				project: { id: projectId },
+			}),
+		);
+	}
+
+	public async create(
+		payload: { apiKey: string } & ActivityLogCreateRequestDto,
+	): Promise<ActivityLogGetAllResponseDto> {
+		const { apiKey, items, userId } = payload;
+
+		const existingProjectApiKey =
+			await this.projectApiKeyService.findByApiKey(apiKey);
+
+		const { projectId } = existingProjectApiKey;
+
+		const createdActivityLogs: ActivityLogGetAllResponseDto = {
+			items: [],
+		};
+
+		for (const record of items) {
+			const { date, items } = record;
+
+			for (const logItem of items) {
+				const activityLog = await this.createActivityLog({
+					date,
+					logItem,
+					projectId,
+					userId,
+				});
+
+				createdActivityLogs.items.push(activityLog.toObject());
+			}
+		}
+
+		return createdActivityLogs;
+	}
+
+	public delete(): ReturnType<Service["delete"]> {
+		return Promise.resolve(true);
+	}
+
+	public find(): ReturnType<Service["find"]> {
+		return Promise.resolve(null);
+	}
+
+	public async findAll(): Promise<ActivityLogGetAllResponseDto> {
+		const activityLogs = await this.activityLogRepository.findAll();
+
+		return {
+			items: activityLogs.items.map((item) => item.toObject()),
+		};
+	}
+
+	public update(): ReturnType<Service["update"]> {
+		return Promise.resolve(null);
+	}
+}
+
+export { ActivityLogService };
