c5cda1df9f7aa3cb5a389fd59e05ded2248378fb
Anna Honcharova <91134528+AnutkaGn@users.noreply.github.com>
2024-09-17 18:55:30 +0300

    feat: add search input to users table on create project group modal gf-355 (#368)
    Co-authored-by: Yelyzaveta Veis <64521658+liza-veis@users.noreply.github.com>

--- apps/frontend/src/pages/project-access-management/libs/components/project-group-create-form/project-group-create-form.tsx
+++ apps/frontend/src/pages/project-access-management/libs/components/project-group-create-form/project-group-create-form.tsx
@@ -7,37 +7,27 @@ import {
 import {
 	type ProjectGroupCreateRequestDto,
 	projectGroupCreateValidationSchema,
-	type ProjectGroupGetAllItemResponseDto,
 	actions as projectGroupsActions,
 } from "~/modules/project-groups/project-groups.js";
 
-import { filterUserProjectGroups } from "../../helpers/helpers.js";
 import { ProjectGroupForm } from "../project-group-form/project-group-form.js";
 import { DEFAULT_PROJECT_GROUP_CREATE_PAYLOAD } from "./libs/constants/constants.js";
 
 type Properties = {
 	onSubmit: (payload: ProjectGroupCreateRequestDto) => void;
-	projectGroups: ProjectGroupGetAllItemResponseDto[];
 	projectId: number;
 };
 
 const ProjectGroupCreateForm = ({
 	onSubmit,
-	projectGroups,
 	projectId,
 }: Properties): JSX.Element => {
 	const dispatch = useAppDispatch();
 
-	const { users, usersTotalCount } = useAppSelector(({ projectGroups }) => ({
+	const { usersTotalCount } = useAppSelector(({ projectGroups }) => ({
 		projectGroups,
-		users: projectGroups.users,
 		usersTotalCount: projectGroups.usersTotalCount,
 	}));
-
-	const usersWithCurrentProjectGroups = filterUserProjectGroups(
-		users,
-		projectGroups,
-	);
 
 	const { page: userPage, pageSize: userPageSize } = usePagination({
 		queryParameterPrefix: "project-group-user",
@@ -61,7 +51,6 @@ const ProjectGroupCreateForm = ({
 			}}
 			onSubmit={onSubmit}
 			submitLabel="Submit"
-			users={usersWithCurrentProjectGroups}
 			validationSchema={projectGroupCreateValidationSchema}
 		/>
 	);
