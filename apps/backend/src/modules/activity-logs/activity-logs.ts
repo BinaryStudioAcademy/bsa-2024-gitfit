33ded61ed53ce40c434bd345da7f51f085af2e67
kolibri753 <kolibri753@gmail.com>
2024-09-17 11:10:23 +0300

    refactor: update projectService import gf-274

--- apps/backend/src/modules/activity-logs/activity-logs.ts
+++ apps/backend/src/modules/activity-logs/activity-logs.ts
@@ -2,20 +2,12 @@ import { logger } from "~/libs/modules/logger/logger.js";
 import { contributorService } from "~/modules/contributors/contributors.js";
 import { gitEmailService } from "~/modules/git-emails/git-emails.js";
 import { projectApiKeyService } from "~/modules/project-api-keys/project-api-keys.js";
-import { ProjectModel } from "~/modules/projects/project.model.js";
-import { ProjectRepository } from "~/modules/projects/project.repository.js";
-import { ProjectService } from "~/modules/projects/project.service.js";
+import { projectService } from "~/modules/projects/projects.js";
 
 import { ActivityLogController } from "./activity-log.controller.js";
 import { ActivityLogModel } from "./activity-log.model.js";
 import { ActivityLogRepository } from "./activity-log.repository.js";
 import { ActivityLogService } from "./activity-log.service.js";
-
-const projectRepository = new ProjectRepository(ProjectModel);
-const projectService = new ProjectService(
-	projectRepository,
-	projectApiKeyService,
-);
 
 const activityLogRepository = new ActivityLogRepository(ActivityLogModel);
 const activityLogService = new ActivityLogService({
