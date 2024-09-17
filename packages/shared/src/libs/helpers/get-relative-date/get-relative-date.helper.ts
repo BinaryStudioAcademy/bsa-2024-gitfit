f4f5b2ad67e63b1b23991f4fcae993fc76098230
kolibri753 <kolibri753@gmail.com>
2024-09-17 18:10:39 +0300

    feat: add a condition to handle date difference less than a day gf-274

--- packages/shared/src/libs/helpers/get-relative-date/get-relative-date.helper.ts
+++ packages/shared/src/libs/helpers/get-relative-date/get-relative-date.helper.ts
@@ -1,11 +1,19 @@
-import { formatDistance } from "date-fns";
+import { differenceInDays, formatDistance } from "date-fns";
 
 const getRelativeDate = (
 	date: Date,
 	baseDate: Date,
 	options?: { addSuffix?: boolean },
 ): string => {
-	return formatDistance(date, baseDate, options);
+	const LESS_THAN_DAY = 0;
+
+	const daysDifference = differenceInDays(baseDate, date);
+
+	if (daysDifference === LESS_THAN_DAY) {
+		return "Updated today";
+	}
+
+	return formatDistance(date, baseDate, { ...options });
 };
 
 export { getRelativeDate };
