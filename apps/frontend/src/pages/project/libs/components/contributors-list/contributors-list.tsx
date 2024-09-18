d105478acba9ad2f6a5aff8131930129de8abaf7
Vitalii Stefaniv <deosplayt@gmail.com>
2024-09-18 13:54:48 +0300

    refactor: moved conditions to separate constants gf-377

--- apps/frontend/src/pages/project/libs/components/contributors-list/contributors-list.tsx
+++ apps/frontend/src/pages/project/libs/components/contributors-list/contributors-list.tsx
@@ -15,12 +15,14 @@ const ContributorsList = ({
 	isLoading,
 }: Properties): JSX.Element => {
 	const hasContributors = contributors.length > EMPTY_LENGTH;
+	const isListShown = !isLoading && hasContributors;
+	const isEmptyPlaceholderShown = !isLoading && !hasContributors;
 
 	return (
 		<div className={styles["container"]}>
 			<h2 className={styles["title"]}>Contributors</h2>
 			{isLoading && <Loader />}
-			{!isLoading && hasContributors && (
+			{isListShown && (
 				<ul className={styles["list"]}>
 					{contributors.map((contributor) => (
 						<li key={contributor.id}>
@@ -29,7 +31,7 @@ const ContributorsList = ({
 					))}
 				</ul>
 			)}
-			{!isLoading && !hasContributors && (
+			{isEmptyPlaceholderShown && (
 				<p className={styles["empty-placeholder"]}>
 					There are no contributors at the moment. Please set up analytics if
 					you haven&#39;t already.
