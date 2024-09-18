036622d1858b86382f9da4966edea8a8e31cf350
Vitalii Stefaniv <deosplayt@gmail.com>
2024-09-18 11:40:23 +0300

    feat: added placeholder to contributors list on project page gf-377

--- apps/frontend/src/pages/project/libs/components/contributors-list/contributors-list.tsx
+++ apps/frontend/src/pages/project/libs/components/contributors-list/contributors-list.tsx
@@ -1,4 +1,5 @@
 import { Loader } from "~/libs/components/components.js";
+import { EMPTY_LENGTH } from "~/libs/constants/constants.js";
 import { type ContributorGetAllItemResponseDto } from "~/pages/project/libs/types/types.js";
 
 import { ContributorCard } from "../components.js";
@@ -13,12 +14,13 @@ const ContributorsList = ({
 	contributors,
 	isLoading,
 }: Properties): JSX.Element => {
+	const hasContributors = contributors.length > EMPTY_LENGTH;
+
 	return (
 		<div className={styles["container"]}>
 			<h2 className={styles["title"]}>Contributors</h2>
-			{isLoading ? (
-				<Loader />
-			) : (
+			{isLoading && <Loader />}
+			{!isLoading && hasContributors && (
 				<ul className={styles["list"]}>
 					{contributors.map((contributor) => (
 						<li key={contributor.id}>
@@ -26,6 +28,12 @@ const ContributorsList = ({
 						</li>
 					))}
 				</ul>
+			)}
+			{!isLoading && !hasContributors && (
+				<p className={styles["empty-placeholder"]}>
+					There are no contributors at the moment. Please set up analytics if
+					you haven&#39;t already.
+				</p>
 			)}
 		</div>
 	);
