7b357c3604daed631f5e2c912eac0f50f8648a61
Mykhailo Pumnia <misha.pumnya@gmail.com>
2024-09-18 10:56:01 +0300

    feat: restrict access to pages and sidebar items gf-350

--- apps/frontend/src/index.tsx
+++ apps/frontend/src/index.tsx
@@ -63,7 +63,9 @@ createRoot(document.querySelector("#root") as HTMLElement).render(
 							},
 							{
 								element: (
-									<ProtectedRoute>
+									<ProtectedRoute
+										routePermissions={[PermissionKey.MANAGE_ALL_PROJECTS]}
+									>
 										<Contributors />
 									</ProtectedRoute>
 								),
@@ -71,7 +73,12 @@ createRoot(document.querySelector("#root") as HTMLElement).render(
 							},
 							{
 								element: (
-									<ProtectedRoute>
+									<ProtectedRoute
+										routePermissions={[
+											PermissionKey.VIEW_ALL_PROJECTS,
+											PermissionKey.MANAGE_ALL_PROJECTS,
+										]}
+									>
 										<Analytics />
 									</ProtectedRoute>
 								),
