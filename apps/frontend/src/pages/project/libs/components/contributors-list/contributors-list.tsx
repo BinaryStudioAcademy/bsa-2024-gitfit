0308e61e921d790664c6740c92e53c63f966ea67
Vitalii Stefaniv <99541789+Vitaliistf@users.noreply.github.com>
2024-09-18 13:58:52 +0300

    feat: added placeholder to contributors list on project page gf-377 (#384)
    * feat: added placeholder to contributors list on project page gf-377
    
    * feat: added more spacing between title and placeholder gf-377
    
    * refactor: moved conditions to separate constants gf-377

--- apps/frontend/src/pages/project/libs/components/contributors-list/contributors-list.tsx
+++ apps/frontend/src/pages/project/libs/components/contributors-list/contributors-list.tsx
@@ -1,4 +1,5 @@
 import { Loader } from "~/libs/components/components.js";
+import { EMPTY_LENGTH } from "~/libs/constants/constants.js";
 import { type ContributorGetAllItemResponseDto } from "~/pages/project/libs/types/types.js";
 
 import { ContributorCard } from "../components.js";
@@ -13,12 +14,15 @@ const ContributorsList = ({
 	contributors,
 	isLoading,
 }: Properties): JSX.Element => {
+	const hasContributors = contributors.length > EMPTY_LENGTH;
+	const isListShown = !isLoading && hasContributors;
+	const isEmptyPlaceholderShown = !isLoading && !hasContributors;
+
 	return (
 		<div className={styles["container"]}>
 			<h2 className={styles["title"]}>Contributors</h2>
-			{isLoading ? (
-				<Loader />
-			) : (
+			{isLoading && <Loader />}
+			{isListShown && (
 				<ul className={styles["list"]}>
 					{contributors.map((contributor) => (
 						<li key={contributor.id}>
@@ -26,6 +30,12 @@ const ContributorsList = ({
 						</li>
 					))}
 				</ul>
+			)}
+			{isEmptyPlaceholderShown && (
+				<p className={styles["empty-placeholder"]}>
+					There are no contributors at the moment. Please set up analytics if
+					you haven&#39;t already.
+				</p>
 			)}
 		</div>
 	);
