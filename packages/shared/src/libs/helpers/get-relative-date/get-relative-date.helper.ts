b451ac0bf93fe582888626e05b99b19013b1f9cc
kolibri753 <kolibri753@gmail.com>
2024-09-18 14:58:26 +0300

    refactor: remove unnecessary constants gf-274

--- packages/shared/src/libs/helpers/get-relative-date/get-relative-date.helper.ts
+++ packages/shared/src/libs/helpers/get-relative-date/get-relative-date.helper.ts
@@ -1,15 +1,13 @@
 import { differenceInDays, formatDistance, isSameDay } from "date-fns";
 
 const getRelativeDate = (date: Date, baseDate: Date): string => {
-	const parsedDate = new Date(date);
-	const parsedBaseDate = new Date(baseDate);
 	const DIFF_IN_A_DAY = 1;
 
-	if (isSameDay(parsedDate, parsedBaseDate)) {
+	if (isSameDay(date, baseDate)) {
 		return "today";
 	}
 
-	if (differenceInDays(parsedBaseDate, parsedDate) === DIFF_IN_A_DAY) {
+	if (differenceInDays(baseDate, date) === DIFF_IN_A_DAY) {
 		return "yesterday";
 	}
 
