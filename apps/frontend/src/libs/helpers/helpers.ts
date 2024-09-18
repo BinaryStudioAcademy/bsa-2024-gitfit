6a555f628199add5140a2c7eec7be6650ead7192
Vitalii Stefaniv <deosplayt@gmail.com>
2024-09-18 16:00:06 +0300

    refactor: fixed code on front-end to match criteria gf-382

--- apps/frontend/src/libs/helpers/helpers.ts
+++ apps/frontend/src/libs/helpers/helpers.ts
@@ -1,5 +1,5 @@
 export { getPermittedNavigationItems } from "./get-permitted-navigation-items/get-permitted-navigation-items.helper.js";
-export { getValidClassNames } from "./get-valid-classes.helper.js";
+export { getValidClassNames } from "./get-valid-classes/get-valid-classes.helper.js";
 export {
 	addDays,
 	checkHasPermission,
