312829860a693a1f075a7f2d031026a7eb0edb5c
Vitalii Stefaniv <99541789+Vitaliistf@users.noreply.github.com>
2024-09-19 18:05:41 +0300

    feat: add last activity time to contributors gf-347 (#410)
    * feat: added last activity retrieving to the backend gf-347
    
    * feat: added last activity displaying to frontend gf-347
    
    * fix: added usage of getStartOfDay to fix the bug gf-387
    
    * refactor: moved indicator enum to common enums gf-387
    
    * fix: fixed formatting after merge gf-347
    
    ---------
    
    Signed-off-by: Vitalii Stefaniv <99541789+Vitaliistf@users.noreply.github.com>

--- apps/frontend/src/libs/helpers/helpers.ts
+++ apps/frontend/src/libs/helpers/helpers.ts
@@ -1,3 +1,4 @@
+export { getActivityIndicatorStatus } from "./get-activity-indicator-status/get-activity-indicator-status.helper.js";
 export { getPermittedNavigationItems } from "./get-permitted-navigation-items/get-permitted-navigation-items.helper.js";
 export { getValidClassNames } from "./get-valid-classes/get-valid-classes.helper.js";
 export {
--- apps/frontend/src/pages/project/libs/components/contributor-card/contributor-card.tsx
+++ apps/frontend/src/pages/project/libs/components/contributor-card/contributor-card.tsx
@@ -1,3 +1,10 @@
+import { ActivityIndicator } from "~/libs/components/components.js";
+import {
+	getActivityIndicatorStatus,
+	getDifferenceInDays,
+	getRelativeDate,
+	getStartOfDay,
+} from "~/libs/helpers/helpers.js";
 import { useCallback } from "~/libs/hooks/hooks.js";
 import { type ContributorGetAllItemResponseDto } from "~/pages/project/libs/types/types.js";
 
@@ -10,6 +17,26 @@ type Properties = {
 };
 
 const ContributorCard = ({ contributor, onEdit }: Properties): JSX.Element => {
+	const currentDate = getStartOfDay(new Date());
+	const lastActivityDate = contributor.lastActivityDate
+		? new Date(contributor.lastActivityDate)
+		: null;
+
+	const daysDifference = lastActivityDate
+		? getDifferenceInDays(currentDate, lastActivityDate)
+		: null;
+
+	const lastUpdateLabel = lastActivityDate
+		? `Updated ${getRelativeDate(lastActivityDate, currentDate)}`
+		: null;
+
+	const colorStatus =
+		typeof daysDifference === "number"
+			? getActivityIndicatorStatus(daysDifference)
+			: null;
+
+	const hasActivityIndicator = lastUpdateLabel !== null && colorStatus !== null;
+
 	const handleEditClick = useCallback(() => {
 		onEdit(contributor.id);
 	}, [onEdit, contributor.id]);
@@ -17,6 +44,9 @@ const ContributorCard = ({ contributor, onEdit }: Properties): JSX.Element => {
 	return (
 		<div className={styles["card"]}>
 			<span className={styles["name"]}>{contributor.name}</span>
+			{hasActivityIndicator && (
+				<ActivityIndicator label={lastUpdateLabel} status={colorStatus} />
+			)}
 			<ContributorMenu
 				contributorId={contributor.id}
 				onEdit={handleEditClick}
