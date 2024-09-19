bd72f6eb5b5d262c3769321e255aace56eeb033a
Vitalii Stefaniv <deosplayt@gmail.com>
2024-09-19 10:44:15 +0300

    feat: added last activity displaying to frontend gf-347

--- apps/backend/src/modules/contributors/contributor.repository.ts
+++ apps/backend/src/modules/contributors/contributor.repository.ts
@@ -73,7 +73,11 @@ class ContributorRepository implements Repository {
 					"COALESCE(ARRAY_AGG(DISTINCT jsonb_build_object('id', projects.id, 'name', projects.name)) FILTER (WHERE projects.id IS NOT NULL), '{}') AS projects",
 				),
 			)
-			.select(raw("MAX(activity_logs.date) AS last_activity_date"))
+			.select(
+				raw(
+					"MAX(CASE WHEN activity_logs.commits_number > 0 THEN activity_logs.date ELSE NULL END) AS last_activity_date",
+				),
+			)
 			.leftJoin("git_emails", "contributors.id", "git_emails.contributor_id")
 			.leftJoin("activity_logs", "git_emails.id", "activity_logs.git_email_id")
 			.leftJoin("projects", "activity_logs.project_id", "projects.id")
--- apps/frontend/src/pages/project/libs/components/contributor-card/contributor-card.tsx
+++ apps/frontend/src/pages/project/libs/components/contributor-card/contributor-card.tsx
@@ -1,4 +1,10 @@
+import { ActivityIndicator } from "~/libs/components/components.js";
+import {
+	getDifferenceInDays,
+	getRelativeDate,
+} from "~/libs/helpers/helpers.js";
 import { type ContributorGetAllItemResponseDto } from "~/pages/project/libs/types/types.js";
+import { getActivityIndicatorStatus } from "~/pages/projects/libs/helpers/helpers.js";
 
 import styles from "./styles.module.css";
 
@@ -7,9 +13,33 @@ type Properties = {
 };
 
 const ContributorCard = ({ contributor }: Properties): JSX.Element => {
+	//TODO: start of the day
+	const currentDate = new Date();
+	const lastActivityDate = contributor.lastActivityDate
+		? new Date(contributor.lastActivityDate)
+		: null;
+
+	const daysDifference = lastActivityDate
+		? getDifferenceInDays(currentDate, lastActivityDate)
+		: null;
+
+	const lastUpdateLabel = lastActivityDate
+		? `Updated ${getRelativeDate(lastActivityDate, currentDate)}`
+		: null;
+
+	const colorStatus =
+		typeof daysDifference === "number"
+			? getActivityIndicatorStatus(daysDifference)
+			: null;
+
+	const hasActivityIndicator = lastUpdateLabel !== null && colorStatus !== null;
+
 	return (
 		<div className={styles["card"]}>
 			<span className={styles["name"]}>{contributor.name}</span>
+			{hasActivityIndicator && (
+				<ActivityIndicator label={lastUpdateLabel} status={colorStatus} />
+			)}
 		</div>
 	);
 };
