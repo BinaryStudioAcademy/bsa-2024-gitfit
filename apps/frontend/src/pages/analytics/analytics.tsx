a9057c7406327b2ddb04070abbf5457612a867ca
AnutkaGn <annaartemivna@gmail.com>
2024-09-20 17:54:45 +0300

    feat: add a select component to the analytics page gf-348

--- apps/frontend/src/pages/analytics/analytics.tsx
+++ apps/frontend/src/pages/analytics/analytics.tsx
@@ -1,18 +1,36 @@
-import { DateInput, PageLayout } from "~/libs/components/components.js";
+import { DateInput, PageLayout, Select } from "~/libs/components/components.js";
 import { subtractDays } from "~/libs/helpers/helpers.js";
-import { useAppForm, useCallback } from "~/libs/hooks/hooks.js";
+import {
+	//useAppDispatch,
+	useAppForm,
+	useAppSelector,
+	useCallback,
+	//	useEffect,
+} from "~/libs/hooks/hooks.js";
 
 import { ANALYTICS_DATE_MAX_RANGE } from "./libs/constants/constants.js";
+import { getProjectOptions } from "./libs/helpers/helpers.js";
 import styles from "./styles.module.css";
+//import { actions as projectActions } from "~/modules/projects/projects.js";
 
 const Analytics = (): JSX.Element => {
 	const todayDate = new Date();
+	//const dispatch = useAppDispatch();
 
-	const { control, handleSubmit } = useAppForm({
+	// useEffect(() => {
+	// 	void dispatch(projectActions.loadAll());
+	// }, [dispatch]);
+
+	const { control, handleSubmit } = useAppForm<{
+		dateRange: [Date, Date];
+		projects: number;
+	}>({
 		defaultValues: {
 			dateRange: [subtractDays(todayDate, ANALYTICS_DATE_MAX_RANGE), todayDate],
+			projects: 0,
 		},
 	});
+	const { projects } = useAppSelector(({ projects }) => projects);
 
 	const handleFormSubmit = useCallback(
 		(event_: React.BaseSyntheticEvent): void => {
@@ -23,11 +41,25 @@ const Analytics = (): JSX.Element => {
 		[handleSubmit],
 	);
 
+	const projectOptions = getProjectOptions(projects);
+
 	return (
 		<PageLayout>
 			<h1 className={styles["title"]}>Analytics</h1>
 			<section>
 				<form className={styles["filters-form"]} onSubmit={handleFormSubmit}>
+					<div className={styles["wraper-selector"]}>
+						<Select
+							control={control}
+							isClearable
+							isLabelHidden
+							isSearchable
+							label="Select project"
+							name="projects"
+							options={projectOptions}
+							placeholder="Select project"
+						/>
+					</div>
 					<DateInput
 						control={control}
 						maxDate={todayDate}
---
+++ apps/frontend/src/pages/analytics/libs/helpers/helpers.ts
@@ -0,0 +1,1 @@
+export { getProjectOptions } from "./get-project-options.helper.js";
