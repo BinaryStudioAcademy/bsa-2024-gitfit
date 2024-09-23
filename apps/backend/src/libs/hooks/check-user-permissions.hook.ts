4614a7a0cd82f3d73502f62a8f14435083ef63ed
Vitalii Stefaniv <deosplayt@gmail.com>
2024-09-23 11:41:13 +0300

    fix: added check for user self-access gf-449

--- apps/backend/src/libs/hooks/check-user-permissions.hook.ts
+++ apps/backend/src/libs/hooks/check-user-permissions.hook.ts
@@ -1,19 +1,27 @@
 import { type FastifyRequest } from "fastify";
 
-import { ExceptionMessage } from "~/libs/enums/enums.js";
+import { APIPath, ExceptionMessage } from "~/libs/enums/enums.js";
 import { checkHasPermission } from "~/libs/helpers/helpers.js";
 import { type APIPreHandler } from "~/libs/modules/controller/controller.js";
 import { HTTPCode, HTTPError } from "~/libs/modules/http/http.js";
 
 const checkUserPermissions = (routePermissions: string[]): APIPreHandler => {
 	return (request: FastifyRequest, _, done): void => {
-		const { user } = request;
+		const { params, url, user } = request;
 
 		if (!user) {
 			throw new HTTPError({
 				message: ExceptionMessage.USER_NOT_FOUND,
 				status: HTTPCode.UNAUTHORIZED,
 			});
+		}
+
+		const resourceId = (params as { id?: string }).id;
+
+		if (url.includes(APIPath.USERS) && Number(resourceId) === user.id) {
+			done();
+
+			return;
 		}
 
 		const hasPermission = checkHasPermission(
