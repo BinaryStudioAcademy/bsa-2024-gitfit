94e6f790b9f67371485c3f7b6068199950598f75
AnutkaGn <annaartemivna@gmail.com>
2024-09-23 12:03:18 +0300

    fix: change folder structure gf-348

--- apps/frontend/src/pages/analytics/libs/helpers/helpers.ts
+++ apps/frontend/src/pages/analytics/libs/helpers/helpers.ts
@@ -1,3 +1,3 @@
 export { getAnalyticsColumns } from "./get-analytics-columns/get-analytics-columns.helper.jsx";
 export { getAnalyticsRows } from "./get-analytics-rows/get-analytics-rows.helper.js";
-export { getProjectOptions } from "./get-project-options.helper.js";
+export { getProjectOptions } from "./get-project-options/get-project-options.helper.js";
