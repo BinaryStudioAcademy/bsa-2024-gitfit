8bc6dd8a0812f2e9dc6866c62ccc62adbf8e7caa
Oleksandra Sydorenko <105361812+sandrvvu@users.noreply.github.com>
2024-09-25 09:32:19 +0300

    feat: decrease default date range to one week gf-503 (#518)
    * feat: decrease default date range to one week gf-503
    
    * feat: add contribution analytics date max range constant gf-503
    
    * fix: separate default and max date range gf-503
    
    * fix: constant path gf-503

--- apps/frontend/src/pages/analytics/libs/constants/constants.ts
+++ apps/frontend/src/pages/analytics/libs/constants/constants.ts
@@ -1,2 +1,3 @@
 export { ANALYTICS_DATE_MAX_RANGE } from "./analytics-date-max-range.constant.js";
+export { ANALYTICS_DEFAULT_DATE_RANGE } from "./analytics-default-date-range.constant.js";
 export { ANALYTICS_LOOKBACK_DAYS_COUNT } from "./analytics-lookback-days-count.constant.js";
