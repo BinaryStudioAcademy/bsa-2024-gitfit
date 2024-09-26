db35237dfb9aacd3092a3c5459314a682876d6ca
sirserg <sirserg05@gmail.com>
2024-09-26 18:38:22 +0300

    feat: add contributor name search preselected on redirection gf-540

--- apps/frontend/src/pages/project/libs/components/contributor-card/contributor-card.tsx
+++ apps/frontend/src/pages/project/libs/components/contributor-card/contributor-card.tsx
@@ -47,6 +47,7 @@ const ContributorCard = ({
 }: Properties): JSX.Element => {
 	const analyticsRoute = configureQueryString(AppRoute.ANALYTICS, [
 		[QueryParameterName.PROJECT_ID, projectId],
+		[QueryParameterName.SEARCH, contributor.name],
 	]);
 
 	const currentDate = getStartOfDay(new Date());
