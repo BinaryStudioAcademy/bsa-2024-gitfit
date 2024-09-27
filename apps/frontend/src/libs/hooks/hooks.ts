5df5bb4c41e1bc7e58c8f554819472eec397ed67
Vitalii Stefaniv <deosplayt@gmail.com>
2024-09-27 12:53:04 +0300

    refactor: fixed inconsistent naming gf-557

--- apps/frontend/src/libs/hooks/hooks.ts
+++ apps/frontend/src/libs/hooks/hooks.ts
@@ -2,7 +2,7 @@ export { useAppDispatch } from "./use-app-dispatch/use-app-dispatch.hook.js";
 export { useAppForm } from "./use-app-form/use-app-form.hook.js";
 export { useAppSelector } from "./use-app-selector/use-app-selector.hook.js";
 export { useHandleClickOutside } from "./use-handle-click-outside/use-handle-click-outside.hook.js";
-export { useInfiniteScroll } from "./use-infinity-scroll/use-infinity-scroll.hook.js";
+export { useInfiniteScroll } from "./use-infinite-scroll/use-infinite-scroll.hook.js";
 export { useIntersectionObserver } from "./use-intersection-observer/use-intersection-observer.hook.js";
 export { useModal } from "./use-modal-state/use-modal-state.hook.js";
 export { usePagination } from "./use-pagination/use-pagination.hook.js";
